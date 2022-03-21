746dc559403df7e892a10f21ab9e8745e405c09b
tdOSX <Unaimend@gmail.com>
2022-03-21 15:29:42 +0100

    Revert "Nothing happend here"
    This reverts commit 074fb3310e57ceb3edb315e798c051e1028be680.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3 
 
+class User():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def toString(self):
+        return f"Hello {self.name} you are {self.age} years old"
 
 def main():
     name = input("Please enter your name: " )
-    print(f"Good bye from Python {name}" )
-    return name
+    age = input("Please enter your age: " )
+    user = User(name, age)
+    
+    print(user.toString())
 
+    return name
 
 def sayGoodBye(name):
     print(f"Goodbye {name}")
