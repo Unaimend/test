cda29fd0fa0fc1f10b5a58d5f714fd052305b739
tdOSX <Unaimend@gmail.com>
2022-03-23 09:00:31 +0100

    Revert "Added a user class and age question"
    This reverts commit 411ca734babf1fdf49d61e5d4607c5a971670bb3.

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3 
 
-class User():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def toString(self):
-        return f"Hello {self.name} you are {self.age} years old"
 
 def main():
     name = input("Please enter your name: " )
-    age = input("Please enter your age: " )
-    user = User(name, age)
-    
-    print(user.toString())
-
+    print(f"Hello from Python {name}" )
     return name
+
 
 def sayGoodBye(name):
     print(f"Goodbye {name}")
